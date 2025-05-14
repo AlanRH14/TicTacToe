521e301375d65afa6fd5ce6e2fd37416cffe612c
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-05-14 16:41:32 -0600

    Add Text in TicTacToe (Error & Current Player)

--- app/src/main/java/com/example/tictactoe/presentation/TicTacToeScreen.kt
+++ app/src/main/java/com/example/tictactoe/presentation/TicTacToeScreen.kt
@@ -3,7 +3,9 @@ package com.example.tictactoe.presentation
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -12,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.tictactoe.presentation.widgets.Board
+import com.example.tictactoe.ui.theme.SpacerPadding
 
 @Composable
 fun TicTacToeScreen(
@@ -28,6 +31,16 @@ fun TicTacToeScreen(
     ) {
         Board(uiState) {
             viewModel.makeMove(it)
+        }
+
+        Spacer(modifier = Modifier.height(SpacerPadding))
+
+        AnimatedVisibility(visible = !uiState.isFinished) {
+            Text("Current Player: ${uiState.currentTurn}")
+        }
+
+        AnimatedVisibility(visible = uiState.error != null) {
+            Text(text = "${uiState.error}")
         }
 
         AnimatedVisibility(visible = uiState.winner != null) {
