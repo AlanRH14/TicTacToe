97e03010342bada3ad8d3ae0c9519cfb35c260b8
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-23 13:13:58 -0600

    Config Koin library part. 18.

--- app/src/main/java/com/example/tictactoe/di/AppModule.kt
+++ app/src/main/java/com/example/tictactoe/di/AppModule.kt
@@ -1,7 +1,9 @@
 package com.example.tictactoe.di
 
+import com.example.tictactoe.presentation.TicTacToeViewModel
 import org.koin.dsl.module
 
 val appModule = module {
 
+    single { TicTacToeViewModel(ticTacToe = get()) }
 }
