f0eba7f68f778687bc7d6f9ed5b738fd231e73d1
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-23 13:08:07 -0600

    Config Koin library part. 9.

--- app/src/main/java/com/example/tictactoe/TicTacToeApplication.kt
+++ app/src/main/java/com/example/tictactoe/TicTacToeApplication.kt
@@ -1,7 +1,20 @@
 package com.example.tictactoe
 
 import android.app.Application
+import com.example.tictactoe.di.ticTacModule
 import dagger.hilt.android.HiltAndroidApp
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.koinApplication
 
 @HiltAndroidApp
-class TicTacToeApplication : Application()
+class TicTacToeApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        koinApplication {
+            androidContext(this@TicTacToeApplication)
+            modules(ticTacModule)
+        }
+    }
+}
