9d85d7916bb540d214c04dddfaaf187f2c46229e
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-23 13:11:03 -0600

    Config Koin library part. 15.

--- app/src/main/java/com/example/tictactoe/di/TicTacToeModule.kt
+++ app/src/main/java/com/example/tictactoe/di/TicTacToeModule.kt
@@ -1,12 +1,8 @@
 package com.example.tictactoe.di
 
-import com.example.tictactoe.domain.TicTacToeHandler
-import com.example.tictactoe.domain.model.TicTacToeData
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
