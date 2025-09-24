b701d05b07a18ea10e7fb57b88a19e98dc64a465
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-24 11:01:05 -0600

    Config MVI in project. 40.

--- app/src/main/java/com/example/tictactoe/presentation/mvi/TicTacToeUIEvent.kt
+++ app/src/main/java/com/example/tictactoe/presentation/mvi/TicTacToeUIEvent.kt
@@ -5,4 +5,5 @@ sealed interface TicTacToeUIEvent {
     data class MakeMove(val move: String) : TicTacToeUIEvent
     data object RestartGame : TicTacToeUIEvent
     data class UpdateText(val text: String) : TicTacToeUIEvent
+    data class UpdateVisibilityText(val isVisible: Boolean): TicTacToeUIEvent
 }
