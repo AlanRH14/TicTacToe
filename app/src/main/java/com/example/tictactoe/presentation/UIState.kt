deecb2a62173a4c2365d54bf3013ad5803e749e8
ALAN ANDRES ROSAS HERNANDEZ <alanarh.14@gmail.com>
2025-09-24 10:57:55 -0600

    Config MVI in project. 37.

--- app/src/main/java/com/example/tictactoe/presentation/UIState.kt
+++ app/src/main/java/com/example/tictactoe/presentation/UIState.kt
@@ -3,6 +3,7 @@ package com.example.tictactoe.presentation
 data class UIState(
     val board: List<MutableList<Char>> = emptyList(),
     val textState: String = "",
+    val isVisibleText: Boolean = false,
     val currentTurn: Char = 'X',
     val error: String? = null,
     val winner: Char? = null,
